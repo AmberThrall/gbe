e3c22adf7b04e7bd590fe19a5e2098e06f02f41f
Amber Thrall <amber.rose.thrall@gmail.com>
2023-05-23 01:30:52 -0700

    operator overloading for registers.

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@
 
 int main(int argc, char **argv) {
     Register hl(0x12, 0xAB);
+    hl = 0xAB12;
 
-    std::cout << "Hello World! hl = " << hl << std::endl;
+    std::cout << "Hello World! hl = " << hl + hl << std::endl;
     std::cout << "Test: " << fmt::format(OPCODES[0x01].format, hl.AsWord()) << std::endl;
+    std::cout << "hl == 0x12AB ? " << (hl == 0x12AB ? "True" : "False") << std::endl;
     
     return 0;
 }
--- src/register.hpp
+++ src/register.hpp
@@ -15,6 +15,78 @@ public:
     byte hi;
     byte lo;
 
+    Register& operator=(const word& rhs) {
+        FromWord(rhs);
+        return *this;
+    }
+
+    Register& operator+=(const word rhs) {
+        word v = AsWord();
+        v += rhs;
+        FromWord(v);
+        return *this;
+    }
+    Register& operator+=(const Register& rhs) {
+        (*this) += rhs.AsWord();
+        return *this;
+    }
+
+    Register& operator-=(const word rhs) {
+        word v = AsWord();
+        v -= rhs;
+        FromWord(v);
+        return *this;
+    }
+    Register& operator-=(const Register& rhs) {
+        (*this) -= rhs.AsWord();
+        return *this;
+    }
+
+    Register& operator++() {
+        (*this) += 1;
+        return *this;
+    }
+    Register operator++(int) {
+        Register old = *this;
+        operator++();
+        return old;
+    }
+    Register& operator--() {
+        (*this) -= 1;
+        return *this;
+    }
+    Register operator--(int) {
+        Register old = *this;
+        operator--();
+        return old;
+    }
+
+    friend Register operator+(Register lhs, const word& rhs) {
+        return Register(lhs.AsWord() + rhs);
+    }
+    friend Register operator+(Register lhs, const Register& rhs) {
+        return Register(lhs.AsWord() + rhs.AsWord());
+    }
+    friend Register operator-(Register lhs, const word& rhs) {
+        return Register(lhs.AsWord() - rhs);
+    }
+    friend Register operator-(Register lhs, const Register& rhs) {
+        return Register(lhs.AsWord() - rhs.AsWord());
+    }
+
+    friend bool operator==(const Register& lhs, const Register& rhs) {
+        return lhs.hi == rhs.hi && lhs.lo == rhs.lo;
+    }
+    friend bool operator==(const Register& lhs, const word rhs) {
+        return lhs == Register(rhs);
+    }
+    friend bool operator!=(const Register& lhs, const Register& rhs) {
+        return !(lhs == rhs);
+    }
+    friend bool operator!=(const Register& lhs, const word rhs) {
+        return !(lhs == rhs);
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const Register& obj) {
         os << fmt::format("0x{:04X}", obj.AsWord());
         return os;
